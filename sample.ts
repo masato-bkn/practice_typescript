d3922366fd5d953f46dc8bc24c80055b03c9e0d6
重田 将人 <masato.0928.j@gmail.com>
2021-04-17 16:42:41 +0900

    hashの型推論

---
+++ sample.ts
@@ -0,0 +1,23 @@
+// 配列の型定義
+let someArray: Array<number> = [1, 3, 5]
+
+// item => インデックス
+for (let item in someArray) {
+    console.log(item);
+}
+
+// item => 要素
+for (let item of someArray) {
+    console.log(item);
+}
+
+someArray.push(9)
+console.log(someArray)
+
+// hashのkey: valueの方をチェックする
+let hash: { [key: string]: string; } = {}; 
+
+hash['a'] = 'a'
+hash[1] = 'a'
+// hash['a'] = 1
+console.log(hash)
