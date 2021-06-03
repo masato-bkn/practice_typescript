24ee834833ea9bc4b3abe3ebc41c3357697fc3c3
重田 将人 <masato.0928.j@gmail.com>
2021-06-03 22:42:13 +0900

    async

--- async.ts
+++ async.ts
@@ -1,3 +1,5 @@
+import { resolvePlugin } from "@babel/core";
+
 let hello = (greeting: string) : Promise<string> => {
     return new Promise((resolve, reject) => {
         if (greeting == "hello") {
@@ -16,3 +18,39 @@ hello("hello").then((res) => {
 
 // =======================================================
 
+const dummyFetch = (path: String, callback: Function) => {
+    setTimeout(() => {
+        if (path.startsWith("/success")) {
+            callback(null, { body: `Response body of ${path}` });
+        } else {
+            callback(new Error("NOT FOUND"));
+        }
+    }, 1000 * Math.random());
+}
+
+dummyFetch("/success/data", (error, data) => {
+    if(error) {
+        // do nothing
+    } else {
+        console.log(data);
+    }
+});
+
+const dummyFetch2 = (path: string): Promise<any> => {
+    return new Promise((resolve, reject) => {
+        if (path.startsWith("/success")) {
+            resolve({ body: `Response body of ${path}` })
+        } else {
+            reject(new Error("NOT FOUND"))
+        }
+    });
+}
+
+dummyFetch2("/success/data").then(
+    (response: any) => {
+        console.log(response)
+    },
+    (error: any) => {
+        console.log(error)
+    },
+);
